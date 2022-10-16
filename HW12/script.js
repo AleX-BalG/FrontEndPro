c0eb5b4816b26b59a9760c3b43b80386652bdd36
AleX-BalG <4otsolnca@gmail.com>
2022-10-17 02:05:08 +0300

    did my homework number 12

---
+++ HW12/script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const calc = new Calculator();
+
+function Calculator(a) {
+    this.result = a;
+    this.sum = function (b) {
+        this.result = a + b;
+        console.log(this.result);
+    };
+    this.sub = function (b) {
+        this.result = a - b;
+        console.log(this.result);
+    };
+    this.mult = function (b) {
+        this.result = a * b;
+        console.log(this.result);
+    };
+    this.div = function (b) {
+        this.result = a / b;
+        console.log(this.result);
+    };
+    this.set = function (b) {
+        this.result = a = b;
+        console.log(this.result);
+    };
+
+}
+
+// calc.sum(5); /// 15
+// calc.mult(10); // 150
+// calc.sub(40); // 110
+// calc.div(10); // 11
+// calc.set(100); //
